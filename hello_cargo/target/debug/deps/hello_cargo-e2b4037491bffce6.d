1a0a56e84aa0c5466cbb9ccb30ffcc58a6585178
Sumin Lee <suminlee@Sumins-MacBook-Pro.local>
2024-03-16 15:55:59 +0900

    rust cargo (bulid) cargo new -> 새로운 프로젝트 생성하기 cargo build -> build 하기, 기본값은 debug cargo build --release -> release 용 build cargo check -> 개발중에 사용해서 컴파일 오류 나는지 체크하기! cargo run -> build, and run etc..

---
+++ hello_cargo/target/debug/deps/hello_cargo-e2b4037491bffce6.d
@@ -0,0 +1,5 @@
+/Users/suminlee/projects/rustbook_kr/hello_cargo/target/debug/deps/libhello_cargo-e2b4037491bffce6.rmeta: src/main.rs
+
+/Users/suminlee/projects/rustbook_kr/hello_cargo/target/debug/deps/hello_cargo-e2b4037491bffce6.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/suminlee/projects/rustbook_kr/hello_cargo/target/debug/hello_cargo: /Users/suminlee/projects/rustbook_kr/hello_cargo/src/main.rs
